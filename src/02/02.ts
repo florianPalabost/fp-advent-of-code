1be187ab16f19299de79792a84a5cc0aa801ad96
Florian <palabostg@gmail.com>
2023-12-04 15:17:38 +0100

    done ex2-part1

--- src/02/02.ts
+++ src/02/02.ts
@@ -12,6 +12,13 @@ interface CubeSet {
   blue: number;
 }
 
+type StateColorSeen = {
+  [key: string]: boolean;
+  red: boolean;
+  green: boolean;
+  blue: boolean;
+};
+
 const dataSplitted = getInputData("02/input.txt");
 const QUESTION_SET: CubeSet = {
   red: 12,
@@ -20,23 +27,44 @@ const QUESTION_SET: CubeSet = {
 };
 
 let result = 0;
+const possibleGames: number[] = [];
 
 dataSplitted.forEach((game: string) => {
   const [gameIdRaw, cubesRaw] = game.split(":");
   const gameId = parseInt(gameIdRaw.replace("Game ", ""));
-
+  console.log(gameId);
   const cubesSet = cubesRaw.split(";");
   let match = true;
 
   cubesSet.forEach((cubeList: string) => {
+    // if one subset don't match the game is not possible
     if (!match) {
       return;
     }
 
     const cubeListSplitted = cubeList.split(", ");
+    let stateColorSeen: StateColorSeen = {
+      red: false,
+      green: false,
+      blue: false,
+    };
 
     cubeListSplitted.forEach((cube: string) => {
-      const [cubeNumber, cubeColor] = cube.split(" ");
+      if (!match) {
+        return;
+      }
+      console.log("cube:", cube);
+      const [cubeNumber, cubeColor] = cube.trim().split(" ");
+      console.log("cubeNumber:", cubeNumber, "cubeColor:", cubeColor);
+      // all colors already seen
+      if (stateColorSeen.red && stateColorSeen.green && stateColorSeen.blue) {
+        return;
+      }
+
+      // already seen current color
+      if (cubeColor in stateColorSeen && stateColorSeen[cubeColor]) {
+        return;
+      }
 
       if (
         cubeColor in QUESTION_SET &&
@@ -46,13 +74,18 @@ dataSplitted.forEach((game: string) => {
           `game id : ${gameId} cubeColor:${cubeColor}, cubeNumber:${cubeNumber} > ${QUESTION_SET[cubeColor]}`
         );
         match = false;
+        return;
       }
+      stateColorSeen[cubeColor] = true;
     });
   });
 
   if (match) {
+    possibleGames.push(gameId);
     result += gameId;
   }
 });
 
 printAnswer(result);
+console.log(possibleGames);
+printAnswer(possibleGames.reduce((a, b) => a + b, 0));

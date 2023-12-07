efdc831e0bdf7f362658ead4821baed073eb7827
Florian <palabostg@gmail.com>
2023-12-07 16:25:24 +0100

    lint utils

--- src/utils.ts
+++ src/utils.ts
@@ -2,8 +2,8 @@
  * Export functions that will be reused in others exercises
  */
 
-import { readFileSync } from "fs";
-import path from "path";
+import { readFileSync } from 'fs';
+import path from 'path';
 
 /**
  * Reads the content of a file at the specified input path and returns an array of strings,
@@ -12,10 +12,10 @@ import path from "path";
  * @param {string} filename - The filename of the input file.
  * @return {string[]} An array of strings, where each element represents a line in the file.
  */
-export function getInputData(filename: string) {
-  const data = readFileSync(path.join(__dirname, filename), "utf-8");
+export function getInputData(filename: string): string[] {
+    const data = readFileSync(path.join(__dirname, filename), 'utf-8');
 
-  return data.split("\n");
+    return data.split('\n');
 }
 
 /**
@@ -24,6 +24,6 @@ export function getInputData(filename: string) {
  * @param {number} answer - The number to print.
  * @return {void} This function does not return a value.
  */
-export function printAnswer(answer: number) {
-  console.log("--> Answer: ", answer);
+export function printAnswer(answer: number): void {
+    console.log('--> Answer: ', answer);
 }

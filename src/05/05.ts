f7a009b6527a48660f59a50a97d0acd93cca67c3
Florian <palabostf@gmail.com>
2023-12-07 22:34:58 +0100

    wip ex5.1

--- src/05/05.ts
+++ src/05/05.ts
@@ -18,20 +18,51 @@ console.log('dataSplitted', dataSplitted);
 let seedToPlanted: number[] = [];
 const mapRegex = new RegExp(/map:/);
 
-dataSplitted.forEach((line: string, index: number) => {
+const mappings: MappingSeed[] = [];
+
+let i = 0;
+
+while (i < dataSplitted.length) {
+    const line = dataSplitted[i];
+
     if (!line || line === '') {
-        return;
+        i++;
+        continue;
     }
 
     // get seed to plant in the first line
-    if (index === 0) {
+    if (i === 0) {
         seedToPlanted = line
             .trim()
             .split(' ')
             .map((n) => parseInt(n))
             .filter((n) => !isNaN(n));
     }
-});
+
+    // get name of the mapping if line contains map
+    if (mapRegex.test(line)) {
+        const [from, to] = line.trim().replace(' map:', '').split('-to-');
+
+        // TODO: get the range
+        console.log('from', from, 'to', to);
+
+        // get range number of current mapping
+        let nextLine = dataSplitted[i + 1];
+        let j = i;
+        while (nextLine !== '') {
+            const [targetRangeStart, sourceRangeStart, length] = nextLine
+                .trim()
+                .split(' ')
+                .map((n) => parseInt(n))
+                .filter((n) => !isNaN(n));
+
+            j++;
+            nextLine = dataSplitted[j];
+        }
+    }
+
+    i++;
+}
 
 console.log('seedsToPlanted', seedToPlanted);
 
